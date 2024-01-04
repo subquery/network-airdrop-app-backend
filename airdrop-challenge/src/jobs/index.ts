8e3ef2f7dd18565cc3c3ed25babfb2b859a3ba8a
Jacob <princewith@gmail.com>
2024-01-04 12:21:50 +0800

    update cronTime

--- airdrop-challenge/src/jobs/index.ts
+++ airdrop-challenge/src/jobs/index.ts
@@ -2,8 +2,18 @@ import { CronJob } from "cron";
 import { checkKYCStatus } from "./kyc-whitelisted";
 import { checkDelegateRecord } from "./delegate-record";
 
+let minute = 0;
+
+function increaseMinute() {
+  minute += 5;
+  if (minute >= 60) {
+    minute = minute % 60;
+  }
+  return minute;
+}
+
 CronJob.from({
-  cronTime: "0 0 * * * *",
+  cronTime: `0 ${increaseMinute()} * * * *`,
   onTick: checkKYCStatus,
   timeZone: "UTC",
   start: true,
@@ -11,9 +21,9 @@ CronJob.from({
 });
 
 CronJob.from({
-  cronTime: "0 5 * * * *",
+  cronTime: `0 ${increaseMinute()} * * * *`,
   onTick: checkDelegateRecord,
   timeZone: "UTC",
   start: true,
-  runOnInit: true,
+  runOnInit: false,
 });
